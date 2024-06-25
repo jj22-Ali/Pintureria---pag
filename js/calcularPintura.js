095335419cfc494b431c9a27e79463a7084b841f
Juan Jose Figueroa <juanjosealifigueroa56@gmail.com>
2024-06-25 11:38:03 -0300

    terminado patologias

---
+++ js/calcularPintura.js
@@ -0,0 +1,28 @@
+const miAplicacion = Vue.createApp({
+    data(){
+        return{
+            resultado: null
+        };
+    },
+    methods:{
+        calcularCantidad(){
+            const ancho = parseFloat(this.$refs.ancho.value)
+            const largo = parseFloat(this.$refs.largo.value)
+            const altura = parseFloat(this.$refs.altura.value)
+
+            if(isNaN(ancho) || isNaN(largo) || isNaN(altura)){
+                alert("porfavor, ingrese numeros validos");
+                return
+            }
+            
+            this.resultado = (ancho*altura*largo)/6;
+    
+        },
+        borrarCampos(){
+            this.$refs.ancho.value = "";
+            this.$refs.largo.value = "";
+            this.$refs.altura.value = "";
+            this.resultado = null;
+        }
+    }
+}).mount("#app");
